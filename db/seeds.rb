a6532424759b1e193bea58c2da5b5001cf8f4e2b
Abhishek Pal <Abhishek@techwinners.in>
2022-12-10 12:10:58 +0530

    Fix all isuses login and logout and styling a webpage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+  User.create!(email: 'abhishek@techwinners.in', password: '123456',
+                password_confirmation: '123456')
+end
