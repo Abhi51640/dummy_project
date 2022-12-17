3b29b30d7c0addcbc668d62c67229cde03115d18
Abhishek Pal <Abhishek@techwinners.in>
2022-12-17 12:30:25 +0530

    Create an Admin Panel and AdminUser and It's Seed Entry

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,6 +9,11 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end
+
+if Rails.env.development?
   User.create!(email: 'abhishek@techwinners.in', password: '123456',
-                password_confirmation: '123456', first_name:'Abhishek', last_name:'Pal')
+               password_confirmation: '123456', first_name: 'Abhishek', last_name: 'Pal')
 end
